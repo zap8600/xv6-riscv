61b139ba42ddd131eafaee26016ee12d03591d68
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-02-02 09:30:30 -0600

    uhoh

--- kernel/vm.c
+++ kernel/vm.c
@@ -27,25 +27,32 @@ kvminit()
   memset(kernel_pagetable, 0, PGSIZE);
 
   // uart registers
+  printf("Mapping UART...\n");
   kvmmap(UART0, UART0, PGSIZE, PTE_R | PTE_W);
 
   // virtio mmio disk interface
+  printf("Mapping VIRTIO...\n");
   kvmmap(VIRTIO0, VIRTIO0, PGSIZE, PTE_R | PTE_W);
 
   // CLINT
+  printf("Mapping CLINT...\n");
   kvmmap(CLINT, CLINT, 0x10000, PTE_R | PTE_W);
 
   // PLIC
+  printf("Mapping PLIC...\n");
   kvmmap(PLIC, PLIC, 0x400000, PTE_R | PTE_W);
 
   // map kernel text executable and read-only.
+  printf("Mapping Test Executable...\n");
   kvmmap(KERNBASE, KERNBASE, (uint64)etext-KERNBASE, PTE_R | PTE_X);
 
   // map kernel data and the physical RAM we'll make use of.
+  printf("Mapping Kernel...\n");
   kvmmap((uint64)etext, (uint64)etext, PHYSTOP-(uint64)etext, PTE_R | PTE_W);
 
   // map the trampoline for trap entry/exit to
   // the highest virtual address in the kernel.
+  printf("Mapping Trampoline...\n");
   kvmmap(TRAMPOLINE, (uint64)trampoline, PGSIZE, PTE_R | PTE_X);
 }
 
